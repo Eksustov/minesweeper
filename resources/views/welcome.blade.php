cdb425a4ea4aa132e63cd29724a9ff73cebbf6e1
Elvis <126756049+Eksustov@users.noreply.github.com>
2025-09-04 13:47:22 +0300

    added a gradient background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h1 class="text-3xl font-bold">Minesweep</h1>
+                    <h1 class="text-3xl font-bold">Minesweeper</h1>
                     <p class="mt-4 text-gray-600">Welcome to the game!</p>
                 </div>
             </div>
