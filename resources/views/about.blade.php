94240d1b5edfbbf3daa2166dac544e5df531c518
Elvis <126756049+Eksustov@users.noreply.github.com>
2025-09-04 13:23:36 +0300

    breeze, login, register

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,16 @@
-@section('title', 'Main page')
+@section('title', 'About us')
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('About us') }}
+        </h2>
+    </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h1 class="text-3xl font-bold">Minesweep</h1>
-                    <p class="mt-4 text-gray-600">Welcome to the game!</p>
+                    <div><span class="font-semibold">Hello. I work on this project. Skbidi skibidi</div>
                 </div>
             </div>
         </div>
