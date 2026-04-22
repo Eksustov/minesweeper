128c4b57c5fefa3480f27cb5c01faf36fb5f74b1
Elvis <126756049+Eksustov@users.noreply.github.com>
2026-04-22 13:36:59 +0300

    landing

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,8 +5,9 @@
             <h3 class="text-2xl font-bold mb-6 text-gray-800 border-b pb-2">Welcome to Minesweeper Live</h3>
             <p> Minesweeper Live is a game of minesweeper but a group of people try to solve it 
                 together to clear the board without any mistakes. </p>
-            <form action="/">
-                <button type="submit"> Go to the main page </button>
+            <img src="/minesweeper-1.gif"/>
+            <a href="/">
+                <button class="w-full bg-white text-indigo-600 font-semibold py-2 rounded-lg hover:bg-gray-100 transition-colors"> Go to the main page </button>
             </form>
         </div>
     </div>
